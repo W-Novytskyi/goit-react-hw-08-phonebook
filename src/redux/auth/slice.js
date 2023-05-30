1f508e37cfe224a15adbbc17b192eea46a3aec35
W-Novytskyi <113388494+W-Novytskyi@users.noreply.github.com>
2023-05-30 16:26:59 +0300

    Update slice.js

--- src/redux/auth/slice.js
+++ src/redux/auth/slice.js
@@ -18,19 +18,30 @@ const authSlice = createSlice({
         state.isLoggedIn = true;
       })
       .addCase(register.rejected, (state, action) => state)
+      .addCase(logIn.pending, (state, action) => state)
       .addCase(logIn.fulfilled, (state, action) => {
         state.user = action.payload.user;
         state.token = action.payload.token;
         state.isLoggedIn = true;
       })
+      .addCase(logIn.rejected, (state, action) => state)
+      .addCase(logOut.pending, (state, action) => state)
       .addCase(logOut.fulfilled, state => {
         state.user = { name: null, email: null };
         state.token = null;
         state.isLoggedIn = false;
       })
+      .addCase(logOut.rejected, (state, action) => state)
+      .addCase(refreshUser.pending, (state, action) => {
+        state.isRefreshing = true;
+      })
       .addCase(refreshUser.fulfilled, (state, action) => {
         state.user = action.payload;
         state.isLoggedIn = true;
+        state.isRefreshing = false;
+      })
+      .addCase(refreshUser.rejected, (state, action) => {
+        state.isRefreshing = false;
       }),
 });
 

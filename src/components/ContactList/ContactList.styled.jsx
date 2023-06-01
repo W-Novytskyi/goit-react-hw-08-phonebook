cea95671b49b6a19a19be258b9b1d516e85c5e31
W-Novytskyi <113388494+W-Novytskyi@users.noreply.github.com>
2023-06-01 21:00:30 +0300

    видалення непотрібних стилів

--- src/components/ContactList/ContactList.styled.jsx
+++ src/components/ContactList/ContactList.styled.jsx
@@ -7,21 +7,3 @@ export const Container = styled.ul`
   max-width: 500px;
   padding: 0 20px;
 `;
-
-export const Button = styled.button`
-  cursor: pointer;
-  background-color: #ffffff;
-  color: #000000;
-  margin-left: 10px;
-  padding: 5px 20px;
-  border: 1px solid #000000;
-  border-radius: 3px;
-  box-shadow: 0 3px 3px rgba(0, 0, 0, 0.25);
-  width: 100px;
-
-  &:active {
-    background-color: #1b8bcd;
-    color: #ffffff;
-    border: none;
-  }
-`;
--- src/components/UserMenu/UserMenu.styled.jsx
+++ src/components/UserMenu/UserMenu.styled.jsx
@@ -9,21 +9,3 @@ export const Wrapper = styled.div`
 export const Username = styled.p`
   font-weight: 700;
 `;
-
-export const Button = styled.button`
-  cursor: pointer;
-  background-color: #ffffff;
-  color: #000000;
-  padding: 5px 20px;
-  margin-left: 25px;
-  border: 1px solid #000000;
-  border-radius: 3px;
-  box-shadow: 0 3px 3px rgba(0, 0, 0, 0.25);
-  width: 130px;
-
-  &:active {
-    background-color: #1b8bcd;
-    color: #ffffff;
-    border: none;
-  }
-`;
